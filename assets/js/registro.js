62d4d18749f9e9fa9f86e4f5cd6a6424997de93a
humberto lopez <9372840+hlo2109@users.noreply.github.com>
2019-01-12 17:31:07 -0500

    base
    archivos bases

---
+++ assets/js/registro.js
@@ -0,0 +1,57 @@
+new Vue({
+    el:".divlogin",
+    data:{        
+        form:{
+            type:0, // 0 = Login , 1 = Registro 2- Recuperar contraseña
+            email:"",
+            password:"", 
+            passwordos:""}
+    },
+    methods:{
+        sendForm(){
+            if(this.validaType()){
+                console.log(this.form);
+            }
+        },
+        validaType(){
+            if(this.form.type==0 && !this.validaEmail && !this.validaPassword){
+                return true;
+            }
+            else if(this.form.type==1 && !this.validaEmail && !this.validaRepetirPassword){
+                return true;
+            }
+            else if(this.form.type==2 && !this.validaEmail){
+                return true;
+            }
+            return false;
+        }
+    },
+    computed:{
+        validaEmail(){
+            var exp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if(exp.test(this.form.email)){
+                return false;
+            } else{
+                return true;
+            }
+        },
+        validaPassword(){
+            var exp = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
+            if(exp.test(this.form.password)){
+                return false;
+            } else{
+                return true;
+            }
+        },
+        validaRepetirPassword(){
+            if(this.form.password==this.form.passwordos){
+                return false;
+            } else{
+                return true;
+            }
+        },
+        title(){
+            return (this.form.type==0)?'Login':(this.form.type==1)?'Registro':'Recuperar contraseña';
+        }
+    }
+});
